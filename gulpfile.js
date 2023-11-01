994fd986c85ea74de4f99327fdb95a14e6d5ee8d
Thu Htoo San <43321350+ThuHtooSan@users.noreply.github.com>
2023-11-01 20:03:41 +0630

    Finish all part of the course

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 const { src, dest, watch } = require('gulp'); 
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+const cleancss = require('gulp-clean-css');
 
 const buildStyles = () =>
     src('shinobi/**/*.scss')
         .pipe(sass())
-        .pipe(dest('css'));  
+        .pipe(purgecss({
+            content: ['*.html']
+        }))
+        .pipe(cleancss())
+        .pipe(dest('css'));
 
 const watchTask = () => 
-    watch(['shinobi/**/*.scss'], { ignoreInitial: false }, buildStyles);
+    watch(['shinobi/**/*.scss', '*.html'], { ignoreInitial: false }, buildStyles);
 
 exports.default = watchTask;
